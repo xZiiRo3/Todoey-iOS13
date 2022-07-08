4381f6be7d8bcff7cf67acb8cd07b33920ae6ac3
Rouben Bautista <rouben.bautista@gmail.com>
2022-07-08 13:39:18 -0400

    -REFACTOR

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -25,91 +25,22 @@ class TodoListViewController: UITableViewController{
 //        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 //            itemArray = items
 //        }
-
-        
-        
         // Do any additional setup after loading the view.
     }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        let item = itemArray[indexPath.row]
-        cell.textLabel?.text = item.title
-    
-        
-        //if itemArray[indexPath.row][1] as! Int  == 0 {
-        
-        cell.accessoryType = item.done ? .checkmark : .none
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        
-        
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        saveItems()
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-    }
-    //MARK - Add New Items
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
-        let alert = UIAlertController(title: "Add New Item?", message: "", preferredStyle: .alert)
-        
-        var textField = UITextField()
-        
-        
-        alert.addTextField { alertTextField in
-            alertTextField.placeholder = "Create New Item"
-            textField = alertTextField
-        }
 
-        
-        let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            
-            //what will happen once user clicks add item
-            print("success")
-            
-            if textField.text! == "" {
-                textField.placeholder = "Entry cannot be empty..."
-                self.present(alert, animated: true, completion: nil)
-            } else {
-                var newItem = Item(context: self.context)
-                newItem.title = textField.text!
-                newItem.done = false
-                self.itemArray.append(newItem)
-                
-               // self.itemArray.append([textField.text!, 0])
-//                self.itemArray.append(textField.text!)
-                self.saveItems()
-            }
-            
-            
-        }
-        
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-        
-    }
-    
+}
+
+
+
+//MARK: - SAVE + LOAD
+
+extension TodoListViewController{
     func saveItems() {
-        
-        
         do {
-            
             try context.save()
         } catch {
             print("Error saving context.... \(error)")
         }
-        
         tableView.reloadData()
     }
     
@@ -122,10 +53,66 @@ class TodoListViewController: UITableViewController{
         }
         tableView.reloadData()
     }
+    
+}
+//MARK: - Adding to List
 
+extension TodoListViewController {
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add New Item?", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            //what will happen once user clicks add item
+            print("success")
+            if textField.text! == "" {
+                textField.placeholder = "Entry cannot be empty..."
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                var newItem = Item(context: self.context)
+                newItem.title = textField.text!
+                newItem.done = false
+                self.itemArray.append(newItem)
+                self.saveItems()
+            }
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
-
+//MARK: - Tableview Methods
+extension TodoListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 //MARK: - Search Bar Methods
 
 extension TodoListViewController: UISearchBarDelegate {
